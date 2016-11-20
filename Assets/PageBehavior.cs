c2b5c7c71695198b53a71ee5906dc7661b96d0dd
Emily Goetz <Emily.Goetz@viacom.com>
2016-11-19 23:43:33 -0500

    added paper and debug text

---
+++ Assets/PageBehavior.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PageBehavior : MonoBehaviour {
+
+	bool isVisible;
+	public Text DebugText;
+
+	// Use this for initialization
+	void Start () {
+		isVisible = false;
+		changeDebugText ("Invisible");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnBecameVisible() {
+		isVisible = true;
+		changeDebugText ("Visible");
+	}
+
+	void OnBecameInvisible() {
+		isVisible = false;
+		changeDebugText ("Invisible");
+	}
+
+	void changeDebugText(string text) {
+		DebugText.text = text;
+	}
+}
